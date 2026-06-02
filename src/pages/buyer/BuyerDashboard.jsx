d76017f6acd4bb17e1f588c3b415b59c581201bb
PrinkleMaheshwari <prinklekellabe1st@gmail.com>
2026-06-02 20:42:45 +0500

    Update comment for BuyerDashboard rendering

--- src/pages/buyer/BuyerDashboard.jsx
+++ src/pages/buyer/BuyerDashboard.jsx
@@ -1,7 +1,7 @@
 import BuyerDashboard from "../../components/dashboard/buyer/BuyerDashboard";
 
 // buyer page wrapper
-// Saleha ka buyer dashboard yahan render ho raha hai
+// buyer dashboard yahan render ho raha hai
 
 function BuyerPage() {
   return <BuyerDashboard />;
